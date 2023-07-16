da9b9a8d8b165104193fe896aecc22ce4687e508
Po-Cheng Yeh <blp100x@hotmail.com>
2023-07-16 23:34:14 +0800

    fix: initial color mode

--- src/app/providers.js
+++ src/app/providers.js
@@ -8,6 +8,7 @@ import theme from "@/lib/theme";
 export const Providers = ({ children }) => {
   return (
       <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode}  />
         {children}
       </ChakraProvider>
   );
