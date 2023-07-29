108c234ab6c427fb772364fb9f8bfdcd3e04648e
Po-Cheng Yeh <blp100x@hotmail.com>
2023-07-29 17:30:54 +0800

    feat: change check box styles

--- src/lib/checkbox-theme.js
+++ src/lib/checkbox-theme.js
@@ -1,5 +1,9 @@
 import { checkboxAnatomy } from "@chakra-ui/anatomy";
-import { createMultiStyleConfigHelpers, defineStyle } from "@chakra-ui/react";
+import {
+  border,
+  createMultiStyleConfigHelpers,
+  defineStyle,
+} from "@chakra-ui/react";
 
 const { definePartsStyle, defineMultiStyleConfig } =
   createMultiStyleConfigHelpers(checkboxAnatomy.keys);
@@ -8,9 +12,28 @@ const customCheckBox = definePartsStyle({
   container: {
     p: "3",
     bg: "lightGrey",
+    borderRadius: "4px",
     _dark: {
       bg: "veryDarkGrey",
+      _hover: {
+        bg: "#635FC740",
+      },
     },
+  },
+  control: {
+    _checked: {
+      bg: "mainPurple",
+      color: "white",
+      borderColor: "mainPurple",
+      _hover: {
+        bg: "mainPurpleHover",
+        borderColor: "mainPurpleHover",
+      },
+    },
+  },
+  icon: {
+    w: "10px",
+    h: "8px",
   },
   label: {
     apply: "textStyles.bodyM",
