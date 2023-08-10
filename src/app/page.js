3b7c0ef07f846d6777b25b069c7c4fc9fbd72a06
Po-Cheng Yeh <blp100x@hotmail.com>
2023-08-11 01:58:31 +0800

    fix: demo bug

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,26 @@
 "use client";
 
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Button, Text, useDisclosure } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { useData } from "./dataProvider";
 import { useEffect } from "react";
+import MobileBoardModal from "@/components/mobile-board-modal";
+import { NewBoardModal } from "@/components/modal";
 
 const Home = () => {
   const { dummyData } = useData();
   const router = useRouter();
+  const {
+    isOpen: isOpenNewBoard,
+    onOpen: onOpenNewBoard,
+    onClose: onCloseNewBoard,
+  } = useDisclosure();
+
+  const pathName = dummyData?.boards[0]?.name;
 
   // add first router for demo
   useEffect(() => {
-    if (dummyData && dummyData.boards[0].name) {
+    if (dummyData && dummyData?.boards[0]?.name) {
       router.push(dummyData.boards[0].name);
     }
   }, [dummyData]);
@@ -25,12 +34,25 @@ const Home = () => {
       height={"calc(100vh - 6rem)"}
       gap={8}
     >
-      <Text textStyle="headingL" textAlign="center" color={"mediumGrey"}>
-        Hi! Please select a board. <br />
-        Or wait a moment.<br />
-        <br />
-        Have a good day.
-      </Text>
+      {pathName && (
+        <Text textStyle="headingL" textAlign="center" color={"mediumGrey"}>
+          Hi! Please select a board. <br />
+          Or wait a moment.
+          <br />
+          Have a good day.
+        </Text>
+      )}
+      {!pathName && (
+        <Text textStyle="headingL" textAlign="center" color={"mediumGrey"}>
+          Hi! There is no board <br />
+          Please create a new one. <br />
+          <br />
+          <Button mx="auto" variant="primaryL" p={6} onClick={onOpenNewBoard}>
+            + Add New Board
+          </Button>
+        </Text>
+      )}
+      <NewBoardModal isOpen={isOpenNewBoard} onClose={onCloseNewBoard} />
     </Box>
   );
 };
